4865cf39f72cbdaa107e89f1241bc9b17d4e68a6
LunarKitsunify <kitsunify@gmail.com>
2025-01-16 22:05:17 +0600

    update

--- Python/ContactManager.py
+++ Python/ContactManager.py
@@ -15,63 +15,63 @@ while True:
 
     user_action = input("\nВыберите действие из списка введя от 1 до 6: ")
 
-#region 1. Добавление нового контакта
+    # region 1. Добавление нового контакта
     if user_action == "1":
         print("\n##### Добавление нового контакта #####\n")
-        
-        contact_name =  input("Введите имя пользователя: ")
+
+        contact_name = input("Введите имя пользователя: ")
 
         if contact_name in db:
             print(f"Такой пользователь уже есть, ошибка!")
             continue
 
-        phone_number =  input("Введите номер телефона пользователя: ")
+        phone_number = input("Введите номер телефона пользователя: ")
 
-        db[contact_name] = phone_number;
+        db[contact_name] = phone_number
 
         print(f"Контакт {contact_name} добавлен!")
-#endregion
+    # endregion
 
-#region 2. Просмотр всех контактов
+    # region 2. Просмотр всех контактов
     elif user_action == "2":
         print("\n##### Просмотр всех контактов #####\n")
 
         for name, number in db.items():
             print(f"{name}: {number}")
-#endregion
+    # endregion
 
-#region 3. Поиск контакта
+    # region 3. Поиск контакта
     elif user_action == "3":
         print("\n##### Поиск контакта #####\n")
 
-        contact_name =  input("Введите имя пользователя для поиска: ")
-        
+        contact_name = input("Введите имя пользователя для поиска: ")
+
         if contact_name in db:
             print(f"Контакт {contact_name}: {db[f'{contact_name}']}")
         else:
             print("Пользователь не найден.")
-#endregion
-    
-#region 4. Изменение контакта
+    # endregion
+
+    # region 4. Изменение контакта
     elif user_action == "4":
         print("\n##### Изменение контакта #####\n")
 
-        contact_name =  input("Введите имя пользователя для изменения: ")
-        
+        contact_name = input("Введите имя пользователя для изменения: ")
+
         if contact_name in db:
-            phone_number =  input("Введите новый номер телефона для пользователя: ")
-            db[contact_name] = phone_number;
+            phone_number = input("Введите новый номер телефона для пользователя: ")
+            db[contact_name] = phone_number
             print(f"Контакт {contact_name} изменен на номер {phone_number}!")
         else:
             print("Контакт не найден.")
-#endregion
+    # endregion
 
-#region 5. Удаление контакта
+    # region 5. Удаление контакта
 
     elif user_action == "5":
         print("\n##### Удаление пользователя #####\n")
 
-        contact_name =  input("Введите имя пользователя для удаления: ")
+        contact_name = input("Введите имя пользователя для удаления: ")
 
         if contact_name in db:
             db.pop(contact_name)
@@ -79,15 +79,15 @@ while True:
         else:
             print("Контакт не найден.")
 
-#endregion
+    # endregion
 
-#region 6.Выход из программы
+    # region 6.Выход из программы
 
     elif user_action == "6":
         print("Выход из программы")
         break
 
-#endregion
+    # endregion
 
     else:
         print("\nВыберите действие из списка введя от 1 до 6:")
